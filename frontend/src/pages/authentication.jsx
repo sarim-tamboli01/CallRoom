3eff746b569c1ddd477c03c12bb0bd854724a6d1
sarim tamboli <sarimtamboli2@gmail.com>
2025-09-21 15:52:40 +0530

    random image update

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -76,7 +76,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                         backgroundImage: `url(https://source.unsplash.com/random/1600x900/?nature,wallpapers&sig=${Math.random()})`,
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
